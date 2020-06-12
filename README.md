a67daf2cb0da865e3db2497f49d37851e89d29d3
Thanh Huy Vũ Phạm <huy.vu@codev.vn>
2020-06-12 22:53:20 +0700

    add source

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# insta-db
+# Strapi application
+
+A quick description of your strapi application
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '244a7728-2846-4bbf-bfd3-1efb7d2754d1'
+};
